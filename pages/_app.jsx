36cf1a4c471cb1da85bde41e051f4d86c0d8f333
Shawn DI <ShawnsuDI@Shawns-MBP13-2018.local>
2022-06-06 15:17:27 +1200

    add: css theme through theme-ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,10 @@
+import { ThemeProvider } from "theme-ui";
+import theme from "../theme.js"
 
-
-export default function App({Component, pageProps}){
-    return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
