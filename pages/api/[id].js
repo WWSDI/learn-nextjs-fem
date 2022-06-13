f4094a071a2e271c00aaec721d21ca2c1ce49879
Shawn DI <ShawnsuDI@Shawns-MBP13-2018.local>
2022-06-13 17:50:50 +1200

    add: CRUD routes for notes

---
+++ pages/api/[id].js
@@ -0,0 +1,34 @@
+import nc from "next-connect";
+import notes from "../../src/data/data";
+
+const getNote = (id) => notes.find((note) => note.id == id);
+const getNoteIdx = (id) => notes.findIndex((note) => note.id == id);
+
+const handler = nc()
+  // GET one note
+  .get((req, res) => {
+    const id = req.query.id;
+    console.log("😍", id);
+    const note = getNote(id);
+    if (!note) return res.status(404). json({ message: "not found" });
+    res.json({ message: "ok", note });
+  })
+  // Update (PATCH) one note
+  .patch((req, res) => {
+    const id = req.query.id;
+    // update the note
+    const idx = getNoteIdx(id)
+    notes[idx] = { ...req.body, id: notes[idx].id };
+    if (idx === -1) return res.status(404).json({ message: "not found" });
+    res.json({ message: "ok", note: notes[idx] });
+  })
+  // DEL one note
+  .delete((req, res) => {
+    const id = req.query.id;
+    const idx = getNoteIdx(id)
+    if(idx===-1) return res.status(404).json({ message: "not found" });
+    notes.splice(idx, 1)
+    res.json({message:"delete success", id})
+  });
+
+export default handler;
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,17 @@
-export default (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'application/json')
-    res.end(JSON.stringify({message: 'hello'}))
-} 
+import nc from "next-connect";
+import notes from "../../src/data/data";
+
+const handler = nc()
+  // GET all ntoes
+  .get((req, res) => {
+    res.json({ message: "ok", notes });
+  })
+  // Create (POST) a new note
+  .post((req, res) => {
+    const id = Date.now();
+    const note = { data: req.body, id };
+    notes.push(note);
+    res.json({ message: "new note created successfully", data: note });
+  });
+
+export default handler;
